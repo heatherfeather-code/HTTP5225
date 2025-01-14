1c62408f15d3f740a27d087c0dae5ae332ea7b09
Heather <heatherboden3@gmail.com>
2025-01-14 18:02:57 -0500

    pushing week 1 folder

---
+++ week1/index.php
@@ -0,0 +1,19 @@
+
+<!-- php does not interfere with the execution of html page -->
+<!-- php can go anywhere within the file, it does not matter -->
+ <!-- because we are using php we cannot just open the file with the file explorer, you need mamp to execute the file -->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php echo "hello world!"; ?>
+    
+</body>
+</html>
